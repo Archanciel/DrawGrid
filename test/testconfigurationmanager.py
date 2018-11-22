13433550abab88150c27eae8d485913cf75dd532
Archanciel <jp.schnyder@gmail.com>
2018-11-22 17:45:53 +0100

    init commit

---
+++ test/testconfigurationmanager.py
@@ -0,0 +1,110 @@
+import unittest
+import os, sys, inspect
+
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir)
+
+from configurationmanager import ConfigurationManager
+
+
+class TestConfigurationManager(unittest.TestCase):
+    def setUp(self):
+        if os.name == 'posix':
+            self.filePath = '/sdcard/gridview_test.ini'
+        else:
+            self.filePath = 'c:\\temp\\gridview_test.ini'
+
+
+    def testConfigurationManagerInstanciation(self):
+        self.configMgr = ConfigurationManager(self.filePath)
+        self.assertEqual(self.configMgr.localTimeZone, 'Europe/Zurich')
+        self.assertEqual(self.configMgr.dateTimeFormat, 'DD/MM/YY HH:mm')
+        self.assertEqual(self.configMgr.dateOnlyFormat, 'DD/MM/YY')
+        
+        if os.name == 'posix':
+            self.assertEqual(self.configMgr.dataPath, '/sdcard/CryptoPricerData')
+            self.assertEqual(self.configMgr.appSize, 'Half')
+            self.assertEqual(self.configMgr.histoListItemHeight, '90')
+        else:
+            self.assertEqual(self.configMgr.dataPath, 'c:\\temp')
+            self.assertEqual(self.configMgr.appSize, 'Full')
+            self.assertEqual(self.configMgr.histoListItemHeight, '35')
+
+        self.assertEqual(self.configMgr.loadAtStartPathFilename, '')
+        self.assertEqual(self.configMgr.histoListVisibleSize, '3')
+        self.assertEqual(self.configMgr.appSizeHalfProportion, '0.56')
+        self.assertEqual(self.configMgr.referenceCurrency, 'USD')
+
+
+    def testConfigurationManagerInstanciationNoConfigFile(self):
+        os.remove(self.filePath)
+        self.configMgr = ConfigurationManager(self.filePath)
+        self.assertEqual(self.configMgr.localTimeZone, 'Europe/Zurich')
+        self.assertEqual(self.configMgr.dateTimeFormat, 'DD/MM/YY HH:mm')
+        self.assertEqual(self.configMgr.dateOnlyFormat, 'DD/MM/YY')
+
+        if os.name == 'posix':
+            self.assertEqual(self.configMgr.dataPath, '/sdcard/CryptoPricerData')
+            self.assertEqual(self.configMgr.appSize, 'Half')
+            self.assertEqual(self.configMgr.histoListItemHeight, '90')
+        else:
+            self.assertEqual(self.configMgr.dataPath, 'c:\\temp')
+            self.assertEqual(self.configMgr.appSize, 'Full')
+            self.assertEqual(self.configMgr.histoListItemHeight, '35')
+
+        self.assertEqual(self.configMgr.loadAtStartPathFilename, '')
+        self.assertEqual(self.configMgr.histoListVisibleSize, '3')
+        self.assertEqual(self.configMgr.appSizeHalfProportion, '0.56')
+        self.assertEqual(self.configMgr.referenceCurrency, 'USD')
+
+
+    def testConfigurationManagerInstanciationEmptyConfigFile(self):
+        open(self.filePath, 'w').close()
+        self.configMgr = ConfigurationManager(self.filePath)
+        self.assertEqual(self.configMgr.localTimeZone, 'Europe/Zurich')
+        self.assertEqual(self.configMgr.dateTimeFormat, 'DD/MM/YY HH:mm')
+        self.assertEqual(self.configMgr.dateOnlyFormat, 'DD/MM/YY')
+
+        if os.name == 'posix':
+            self.assertEqual(self.configMgr.dataPath, '/sdcard/CryptoPricerData')
+            self.assertEqual(self.configMgr.appSize, 'Half')
+            self.assertEqual(self.configMgr.histoListItemHeight, '90')
+        else:
+            self.assertEqual(self.configMgr.dataPath, 'c:\\temp')
+            self.assertEqual(self.configMgr.appSize, 'Full')
+            self.assertEqual(self.configMgr.histoListItemHeight, '35')
+
+        self.assertEqual(self.configMgr.loadAtStartPathFilename, '')
+        self.assertEqual(self.configMgr.histoListVisibleSize, '3')
+        self.assertEqual(self.configMgr.appSizeHalfProportion, '0.56')
+        self.assertEqual(self.configMgr.referenceCurrency, 'USD')
+
+
+    def testConfigurationManagerInstanciationOneMissingKey(self):
+        #removing second line in config file
+        with open(self.filePath, 'r') as configFile:
+            lines = configFile.readlines()
+
+        with open(self.filePath, 'w') as configFile:
+            # first line contains [General] section name !
+            configFile.write(''.join(lines[0:1] + lines[2:]))
+
+        self.configMgr = ConfigurationManager(self.filePath)
+        self.assertEqual(self.configMgr.localTimeZone, 'Europe/Zurich')
+        self.assertEqual(self.configMgr.dateTimeFormat, 'DD/MM/YY HH:mm')
+        self.assertEqual(self.configMgr.dateOnlyFormat, 'DD/MM/YY')
+
+        if os.name == 'posix':
+            self.assertEqual(self.configMgr.dataPath, '/sdcard/CryptoPricerData')
+        else:
+            self.assertEqual(self.configMgr.dataPath, 'c:\\temp')
+
+        self.assertEqual(self.configMgr.loadAtStartPathFilename, '')
+        self.assertEqual(self.configMgr.histoListVisibleSize, '3')
+        self.assertEqual(self.configMgr.appSizeHalfProportion, '0.56')
+        self.assertEqual(self.configMgr.referenceCurrency, 'USD')
+
+
+if __name__ == '__main__':
+    unittest.main()
